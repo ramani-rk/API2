80699abc61c00ffb95e5575e5d9dcf19d241e6fe
ramani-rk <ramanikanthsasi@gmail.com>
2024-03-05 19:50:26 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -9,11 +9,18 @@ from rest_framework.response import Response
 
 
 class ProductCrud(APIView):
-    def get(self,request):
-        PDO=Product.objects.all()
-        PJD=ProductModelSerializers(PDO,many=True)
+
+# get method is used for creating the objects
+
+    def get(self,request,Pro_id):
+        #PDO=Product.objects.all()
+        #PJD=ProductModelSerializers(PDO,many=True)
+
+        PDO=Product.objects.get(Pro_id=Pro_id)
+        PJD=ProductModelSerializers(PDO)
         return Response(PJD.data)
-    
+
+# post method is used for Insert the objects into the Database 
     def post(self,request):
         PJDO=request.data
         PDO=ProductModelSerializers(data=PJDO)
@@ -22,3 +29,36 @@ class ProductCrud(APIView):
             return Response({'Data':'Data inserted successfully'})
         else:
             return Response({'Error':'Data is not inserted'})
+
+
+# put method is used for updating the objects.
+# if you want update the one column also it needs the all the columns if it is(non-updated column also),otherwise it throw error   
+    def put(self,request,Pro_id):
+        PO=Product.objects.get(Pro_id=Pro_id)
+        PDO=ProductModelSerializers(PO,data=request.data)
+        if PDO.is_valid():
+            PDO.save()
+            return Response({'Update':'Data is Updated Successfully'})
+        
+        else:
+            return Response({'Error':'Data is not Updated'})
+
+
+# patch method is used for updating the objects.
+# if you want update the one column we can give non-updated column also, it doesn't throw any error   
+    def patch(self,request,Pro_id):
+        PO=Product.objects.get(Pro_id=Pro_id)
+        PDO=ProductModelSerializers(PO,data=request.data,partial=True) #Partial is used for allows to updating the some columns 
+        
+        if PDO.is_valid():
+            PDO.save()
+            return Response({'Update':'Data is Updated Successfully'})
+        
+        else:
+            return Response({'Error':'Data is not Updated'})
+
+
+# Delete is used for deleting the product
+    def delete(self,request,Pro_id):
+        Product.objects.get(Pro_id=Pro_id).delete()
+        return Response ({'Delete':'Data is Deleted Successfully'})
--- project2/urls.py
+++ project2/urls.py
@@ -20,5 +20,5 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ProductCrud/',ProductCrud.as_view(),name='ProductCrud'),
+    path('ProductCrud/<Pro_id>/',ProductCrud.as_view(),name='ProductCrud'),
 ]
